c93d118d3b947d38ff39952fc7556f4743ecbfe1
WTFPUn <punn_namw@hotmail.com>
2022-02-18 20:18:26 +0700

    add: scroll mouse custom(Thanks @riflowth) fix: Trend card size

--- src/components/home/TrendCard.js
+++ src/components/home/TrendCard.js
@@ -1,6 +1,6 @@
 export default function TrendCard({viewCount, title, subTitle, categorie, author, imageUrl}) {
   return(
-    <div className={`bg-[url('./../../public/homeimage/TrendCard/goodBoy.png')] bg-contain bg-no-repeat flex flex-col h-[24.06rem] w-[17.5rem] shrink-0 truncate my-8  `}>
+    <div className={`bg-[url('./../../public/homeimage/TrendCard/goodBoy.png')] bg-contain bg-no-repeat flex flex-col h-[16.06rem] w-[13.8rem] shrink-0 truncate my-8  `}>
         <div className="text-right mr-3">{viewCount}</div>
         <div className="mt-auto ml-3">{title}</div>
         <div className="ml-3" >{subTitle}</div>
--- src/components/home/TrendCardTab.js
+++ src/components/home/TrendCardTab.js
@@ -5,7 +5,7 @@ export default function TrendCardTab() {
   return(
     <div className="flex flex-col mx-48  my-2"> 
       <div className="big-device:text-3xl">Trending 🔥</div>
-      <div className="flex flex-row text-white  justify-items-center overflow-x-auto gap-8">
+      <div className="flex flex-row text-white  justify-items-center overflow-x-auto gap-1 scroll-smooth">
         <TrendCard viewCount={3000} title={"กระทู้"} subTitle={"ฮาวทู บลาๆๆๆๆๆ"} categorie={"วิชา Toelf", "วิชา Ielf"} author={"สมชาย"} imageUrl={'./../../public/homeimage/TrendCard/goodBoy.png'}/>
         <TrendCard viewCount={3000200} title={"กระทู้"} subTitle={"ฮาวทู เทรน"} categorie={"วิชา ภาษาไทย"} author={"สมชาย2"} imageUrl={'./../../public/homeimage/TrendCard/pencilAndHisShit.png'}/>
         <TrendCard viewCount={3000458} title={"VDO"} subTitle={"ฮาวทู บีอะกู๊ดกาย"} categorie={"วิชา การเงิน", "วิชา เคมี"} author={"สมสาว"} imageUrl={'./../../public/homeimage/TrendCard/graph.png'}/>
