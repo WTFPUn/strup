283127e65114aa50d31f9865a07a4a97fafc1b7f
WTFPUn <punn_namw@hotmail.com>
2022-02-18 19:29:49 +0700

    fix: Trend Card not show in product

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import OSRank from '../components/home/OSRank'
+import TrendCard from '../components/home/TrendCard'
+import TrendCardTab from '../components/home/TrendCardTab'
+import WhatOutSource from '../components/home/whatOutSource'
 import IndexBanner from '../components/IndexBanner'
 
 export default function Home() {
   return (
-    <div className="font-body w-full h- mobile:w-screen medium-tablet:w-screen  big-device:w-full relative ">
-      <div className="sticky top-0 z-50">
+    <div className="font-body mobile:w-screen medium-tablet:w-screen  big-device:w-screen relative ">
       <Header/>
-      </div>
-      <div className="absolute top-0 w-screen">
-       <IndexBanner/>
-       <OSRank/>
+      <div className="absolute top-0 w-screen overflow-y-auto flex-1">
+        <IndexBanner/>
+        <OSRank/>
+        <WhatOutSource/>
+        <TrendCardTab/>
         <Footer/> 
       </div>
       
